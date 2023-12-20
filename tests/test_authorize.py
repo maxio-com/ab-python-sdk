88705a9af05f13345e435ea66b668a687cafc204
Tomasz Cichocki <jaszczuros@gmail.com>
2023-12-20 12:03:15 +0100

    Authorization tests (#6)
    Co-authored-by: Tomasz Cichocki <tomasz.cichocki@boldare.com>

---
+++ tests/test_authorize.py
@@ -0,0 +1,43 @@
+import pytest
+from os import getenv
+
+from apimatic_core.exceptions.auth_validation_exception import AuthValidationException
+
+from advancedbilling.advanced_billing_client import AdvancedBillingClient
+from advancedbilling.exceptions.api_exception import APIException
+from advancedbilling.models.site_response import SiteResponse
+
+from tests.client import create_client
+
+
+class TestAuthorization():
+    def test_no_token_or_password_to_client_create(self):
+        with (pytest.raises(AuthValidationException)):
+            client = AdvancedBillingClient(
+                basic_auth_user_name="token",
+                basic_auth_password=None
+            )
+            client.sites.read_site()
+
+        with (pytest.raises(AuthValidationException)):
+            client = AdvancedBillingClient(
+                basic_auth_user_name=None,
+                basic_auth_password="password"
+            )
+            client.sites.read_site()
+
+    def test_unauthorized(self):
+        client = AdvancedBillingClient(
+            subdomain=getenv("SUBDOMAIN"),
+            domain=getenv("DOMAIN"),
+            basic_auth_user_name="thisiswrongapitokenthisiswrongapitokenV8",
+            basic_auth_password=getenv("BASIC_AUTH_PASSWORD")
+        )
+
+        with (pytest.raises(APIException)):
+            client.sites.read_site()
+
+    def test_authorized(self):
+        result = create_client().sites.read_site()
+
+        assert isinstance(result, SiteResponse)
