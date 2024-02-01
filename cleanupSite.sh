1546a7671b9b731baef8b3195ebc4b07d72399ba
Maciej Nedza <maciej.nedza@keen.io>
2024-02-01 16:34:23 +0100

    test

--- cleanupSite.sh
+++ cleanupSite.sh
@@ -1,2 +1,2 @@
 auth=$(echo "$BASIC_AUTH_USERNAME":"$BASIC_AUTH_PASSWORD" | base64)
-curl -i --verbose --request POST --url https://python-sdk.staging-chargify.com/sites/clear_data.json --header "Authorization: $auth"
+curl -i --verbose --request POST --url https://python-sdk.staging-chargify.com/sites/clear_data.json --header "Authorization: Basic $auth"
