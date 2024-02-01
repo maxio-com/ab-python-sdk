f314d1eac9885671261d3dee2ef0ea796eb97813
Maciej Nedza <maciej.nedza@keen.io>
2024-02-01 16:03:26 +0100

    test

--- cleanupSite.sh
+++ cleanupSite.sh
@@ -2,6 +2,7 @@ auth=$(echo "$BASIC_AUTH_USERNAME":"$BASIC_AUTH_PASSWORD" | base64)
 echo $SUBDOMAIN
 echo $DOMAIN
 echo "$SUBDOMAIN"."$DOMAIN" | base64
+echo "$BASIC_AUTH_USERNAME"."$BASIC_AUTH_PASSWORD" | base64
 curl -i --verbose --request POST \
 --url https://"$SUBDOMAIN"."$DOMAIN"/sites/clear_data.json \
 --header "Authorization: Basic $auth" \
