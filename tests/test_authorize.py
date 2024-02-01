cd6c92e7936e87ca71b963343436fef5a70402a9
Marek Szydłowski <marek.szydlowski@boldare.com>
2024-02-01 16:45:53 +0100

    feat: authorize tests improvements

--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -1,82 +1,66 @@
-from os import getenv
-
 import pytest
 from apimatic_core.exceptions.auth_validation_exception import AuthValidationException
 
 from advancedbilling.advanced_billing_client import AdvancedBillingClient
 from advancedbilling.exceptions.api_exception import APIException
 from advancedbilling.models.site_response import SiteResponse
-from tests.base import TestBase
 
 
-class TestAuthorization(TestBase):
+class TestAuthorization:
     def test_no_token_or_password_to_client_create(self):
-        with (pytest.raises(AuthValidationException)):
-            client = AdvancedBillingClient(
-                basic_auth_user_name="token",
-                basic_auth_password=""
-            )
+        with pytest.raises(AuthValidationException):
+            client = AdvancedBillingClient(basic_auth_user_name="token", basic_auth_password="")
             client.sites.read_site()
 
-        with (pytest.raises(AuthValidationException)):
-            client = AdvancedBillingClient(
-                basic_auth_user_name="",
-                basic_auth_password="password"
-            )
+        with pytest.raises(AuthValidationException):
+            client = AdvancedBillingClient(basic_auth_user_name="", basic_auth_password="password")
             client.sites.read_site()
 
-    def test_unauthorized(self):
-        client = AdvancedBillingClient(
-            subdomain=getenv("SUBDOMAIN"),
-            domain=getenv("DOMAIN"),
-            basic_auth_user_name="thisiswrongapitokenthisiswrongapitokenV8",
-            basic_auth_password=getenv("BASIC_AUTH_PASSWORD")
-        )
+    def test_unauthorized(self, unauthorized_client: AdvancedBillingClient):
+        with pytest.raises(APIException):
+            unauthorized_client.sites.read_site()
 
-        with (pytest.raises(APIException)):
-            client.sites.read_site()
-
-    def test_authorized(self):
-        result = self.client.sites.read_site()
+    def test_authorized(self, client: AdvancedBillingClient):
+        result = client.sites.read_site()
 
         assert isinstance(result, SiteResponse)
 
         site = result.site
-        assert 4502 == site.id
-        assert 'Python SDK Env' == site.name
-        assert 'python-sdk' == site.subdomain
-        assert 'USD' == site.currency
-        assert 722159 == site.seller_id
-        assert True == site.relationship_invoicing_enabled
-        assert False == site.customer_hierarchy_enabled
-        assert False == site.whopays_enabled
-        assert 'self-ungrouped' == site.whopays_default_payer
-        assert True == site.test
-        assert 'automatic' == site.default_payment_collection_method
+        assert site.id == 4502
+        assert site.name == "Python SDK Env"
+        assert site.subdomain == "python-sdk"
+        assert site.currency == "USD"
+        assert site.seller_id == 722159
+        assert site.relationship_invoicing_enabled is True
+        assert site.customer_hierarchy_enabled is False
+        assert site.whopays_enabled is False
+        assert site.whopays_default_payer == "self-ungrouped"
+        assert site.test is True
+        assert site.default_payment_collection_method == "automatic"
 
         allocation_settings = site.allocation_settings
-        assert 'true' == allocation_settings.accrue_charge
-        assert 'prorated' == allocation_settings.upgrade_charge
-        assert 'none' == allocation_settings.downgrade_credit
+        assert allocation_settings.accrue_charge == "true"
+        assert allocation_settings.upgrade_charge == "prorated"
+        assert allocation_settings.downgrade_credit == "none"
 
         organization_address = site.organization_address
-        assert 'Asdf Street' == organization_address.street
-        assert '123/444' == organization_address.line_2
-        assert 'San Antonio' == organization_address.city
-        assert 'TX' == organization_address.state
-        assert '78015' == organization_address.zip
-        assert 'US' == organization_address.country
-        assert 'Developer Experience' == organization_address.name
-        assert '555 111 222' == organization_address.phone
+        assert organization_address.street == "Asdf Street"
+        assert organization_address.line_2 == "123/444"
+        assert organization_address.city == "San Antonio"
+        assert organization_address.state == "TX"
+        assert organization_address.zip == "78015"
+        assert organization_address.country == "US"
+        assert organization_address.name == "Developer Experience"
+        assert organization_address.phone == "555 111 222"
 
         tax_configuration = site.tax_configuration
-        assert 'custom' == tax_configuration.kind
-        assert 'shipping_then_billing' == tax_configuration.destination_address
-        assert False == tax_configuration.fully_configured
+        assert tax_configuration.kind == "custom"
+        assert tax_configuration.destination_address == "shipping_then_billing"
+        assert tax_configuration.fully_configured is False
 
         net_terms = site.net_terms
-        assert 0 == net_terms.default_net_terms
-        assert 0 == net_terms.automatic_net_terms
-        assert 0 == net_terms.remittance_net_terms
-        assert False == net_terms.net_terms_on_remittance_signups_enabled
-        assert False == net_terms.custom_net_terms_enabled
+        assert net_terms.default_net_terms == 0
+        assert net_terms.automatic_net_terms == 0
+        assert net_terms.remittance_net_terms == 0
+        assert net_terms.net_terms_on_remittance_signups_enabled is False
+        assert net_terms.custom_net_terms_enabled is False
