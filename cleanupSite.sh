75d94b80ab3fc272b3c1e539e46a40ebd09fbf9e
Maciej Nedza <maciej.nedza@keen.io>
2024-02-01 15:43:08 +0100

    verbose curl

--- cleanupSite.sh
+++ cleanupSite.sh
@@ -1,7 +1,7 @@
 auth=$(echo "$BASIC_AUTH_USERNAME":"$BASIC_AUTH_PASSWORD" | base64)
 echo $SUBDOMAIN
 echo $DOMAIN
-curl -i --request POST \
+curl -i --verbose --request POST \
 --url https://"$SUBDOMAIN"."$DOMAIN"/sites/clear_data.json \
 --header "Authorization: Basic $auth" \
 --header 'Content-Type: application/json'
