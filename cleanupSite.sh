73ce5437ce3c0f7fca78df760e32bd13de0c7228
Maciej Nedza <maciej.nedza@keen.io>
2024-02-01 15:33:37 +0100

    Test ci

--- cleanupSite.sh
+++ cleanupSite.sh
@@ -1,6 +1,6 @@
 auth=$(echo "$BASIC_AUTH_USERNAME":"$BASIC_AUTH_PASSWORD" | base64)
 
 curl --request POST \
-  --url https://"$SUBDOMAIN"."$DOMAIN"/sites/clear_data.json?cleanup_scope=all \
+  --url https://"$SUBDOMAIN"."$DOMAIN"/sites/clear_data.json \
   --header "Authorization: Basic $auth" \
   --header 'Content-Type: application/json'
